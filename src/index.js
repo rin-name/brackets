7a0f471e2f3216c32f71a84f6f570a9e5554703d
rin-name <marinka_kostyuk@inbox.ru>
2020-10-21 11:07:51 +0300

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        var br = false;
+        let arr = bracketsConfig.map( (e) => e.join("") );
+        for (let i=0;i<arr.length;i++){
+            if(str.includes(arr[i])){
+                str=str.replace(arr[i],"");
+                i =-1;
+            }
+        }
+        if(str.length === 0) {
+            br = true
+        }
+
+        return br;
 }
